3773a41979709352a0055fe6840573d5d7c67be1
Ugwu Collins <collinsuchinaka@gmail.com>
2020-07-14 16:34:47 +0100

    device gem added and installed in the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'homes#index'
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'signup', to: 'devise/registrations#new', as: :new_user
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
